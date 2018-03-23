6267c1a67eb00a2dfd04eca1eda2741367f48921
tomato0228 <1273800239@qq.com>
2018-03-23 20:22:29 +0800

    创建工程

---
+++ src/com/njust/ErrorMessage.java
@@ -0,0 +1,12 @@
+package com.njust;
+
+/**
+ * 错误信息类
+ *
+ * @author tomato
+ * @create 2018-03-21 下午10:07
+ */
+public class ErrorMessage {
+    public static final String CREATNFA = "创建DFA错误";
+    public static final String NFATODFA = "NFA确定化错误";
+}
